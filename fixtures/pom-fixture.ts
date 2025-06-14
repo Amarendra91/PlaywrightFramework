b228b4c787f897f23d0a227eca53b35cf3675b62
Amarendra91 <amarendra867@gmail.com>
2025-06-14 21:11:43 +0530

    Implement fixture for login page object

---
+++ fixtures/pom-fixture.ts
@@ -0,0 +1,14 @@
+import { test as baseTest } from '@playwright/test';
+import { LoginPage } from '../pages/LoginPage';
+
+type PomFixtures = {
+  loginPage: LoginPage;
+};
+
+export const test = baseTest.extend<PomFixtures>({
+  loginPage: async ({ page }, use) => {
+    await use(new LoginPage(page));
+  },
+});
+
+export { expect } from '@playwright/test';
