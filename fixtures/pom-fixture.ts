088e7f89f72728c1dc6ddde491014ecdf40b2251
Amarendra91 <amarendra867@gmail.com>
2025-06-30 00:04:01 +0530

    added PimPage into POM fixture

--- fixtures/pom-fixture.ts
+++ fixtures/pom-fixture.ts
@@ -2,11 +2,13 @@ import { test as baseTest } from '@playwright/test';
 import { HomePage } from '../pages/HomePage';
 import { DashboardPage } from '../pages/DashboardPage';
 import { LeftNavigationPage } from '../pages/LeftNavigationPage';
+import { PimPage } from '../pages/PimPage';
 
 type PomFixtures = {
   homePage: HomePage;
   dashboardpage: DashboardPage;
   leftNavPage: LeftNavigationPage;
+  pimPage: PimPage;
 };
 
 export const test = baseTest.extend<PomFixtures>({
@@ -20,5 +22,9 @@ export const test = baseTest.extend<PomFixtures>({
 
   leftNavPage: async ({ page }, use) => {
     await use(new LeftNavigationPage(page));
+  },
+
+  pimPage: async ({ page }, use) => {
+    await use(new PimPage(page));
   },
 });
