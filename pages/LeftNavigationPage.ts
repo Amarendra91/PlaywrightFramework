7cb85a281576fcc06bb6008c04d9f7d75fede064
Amarendra91 <amarendra867@gmail.com>
2025-06-29 12:20:51 +0530

    left hand nav bar page object file added

---
+++ pages/LeftNavigationPage.ts
@@ -0,0 +1,15 @@
+import { Locator, Page } from '@playwright/test';
+
+export class LeftNavigationPage {
+  readonly page: Page;
+  readonly pimLink: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.pimLink = page.getByRole('link', { name: 'PIM' });
+  }
+
+  async openPimModule(): Promise<void> {
+    this.pimLink.click();
+  }
+}
