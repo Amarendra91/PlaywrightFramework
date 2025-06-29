500ad88afbedea7d6be89734c0d3e7b859a570f1
Amarendra91 <amarendra867@gmail.com>
2025-06-29 21:37:10 +0530

    Added test.describe block, added test tag, added test.step to improve the readability of test reports

--- tests/dashboard-module.spec.ts
+++ tests/dashboard-module.spec.ts
@@ -1,15 +1,26 @@
 import { test, expect } from '../fixtures/hooks-fixture';
 import dashboardModuleTestData from '../test-data/dashboard-module.json';
 
-test('Verify user navigated to OrangeHRM dashbaord page and logout from OrangeHRM application.', async ({
-  dashboardpage,
-  homePage,
-}) => {
-  await expect(dashboardpage.headerTitle).toContainText(
-    dashboardModuleTestData.header_title
+test.describe('Verify Dashbaoard page functionality.', async () => {
+  test(
+    'Verify user navigated to OrangeHRM dashbaord page and logout from OrangeHRM application.',
+    { tag: '@Smoke' },
+    async ({ dashboardpage, homePage }) => {
+      await test.step('Verify page header title.', async () => {
+        await expect(dashboardpage.headerTitle).toContainText(
+          dashboardModuleTestData.header_title
+        );
+      });
+
+      await test.step('Verify user dropdown menu functionality on page header.', async () => {
+        await expect(dashboardpage.userIconMenu).toBeVisible();
+        await expect(dashboardpage.userIconMenu).toBeEnabled();
+      });
+
+      await test.step('Verify Logout functionality. ', async () => {
+        await dashboardpage.logout();
+        await expect(homePage.loginHeader).toBeVisible();
+      });
+    }
   );
-  await expect(dashboardpage.userIconMenu).toBeVisible();
-  await expect(dashboardpage.userIconMenu).toBeEnabled();
-  await dashboardpage.logout();
-  await expect(homePage.loginHeader).toBeVisible();
 });
