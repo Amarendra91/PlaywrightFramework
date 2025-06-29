973cb6948065f553a20c84bbc8b24f79381b95c3
Amarendra91 <amarendra867@gmail.com>
2025-06-29 19:16:13 +0530

    removed unused fixture and added header title text match

--- tests/dashboard-module.spec.ts
+++ tests/dashboard-module.spec.ts
@@ -1,10 +1,13 @@
 import { test, expect } from '../fixtures/hooks-fixture';
+import dashboardModuleTestData from '../test-data/dashboard-module.json';
 
 test('Verify user navigated to OrangeHRM dashbaord page and logout from OrangeHRM application.', async ({
-  goToLoginPage,
   dashboardpage,
   homePage,
 }) => {
+  await expect(dashboardpage.headerTitle).toContainText(
+    dashboardModuleTestData.header_title
+  );
   await expect(dashboardpage.userIconMenu).toBeVisible();
   await expect(dashboardpage.userIconMenu).toBeEnabled();
   await dashboardpage.logout();
