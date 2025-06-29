23331045bf1c8cc846b83ff562e6f06b9c19c518
Amarendra91 <amarendra867@gmail.com>
2025-06-29 12:21:45 +0530

    left hand nav page fixture added

--- fixtures/pom-fixture.ts
+++ fixtures/pom-fixture.ts
@@ -1,10 +1,12 @@
 import { test as baseTest } from '@playwright/test';
 import { HomePage } from '../pages/HomePage';
 import { DashboardPage } from '../pages/DashboardPage';
+import { LeftNavigationPage } from '../pages/LeftNavigationPage';
 
 type PomFixtures = {
   homePage: HomePage;
   dashboardpage: DashboardPage;
+  leftNavPage: LeftNavigationPage;
 };
 
 export const test = baseTest.extend<PomFixtures>({
@@ -14,5 +16,9 @@ export const test = baseTest.extend<PomFixtures>({
 
   dashboardpage: async ({ page }, use) => {
     await use(new DashboardPage(page));
+  },
+
+  leftNavPage: async ({ page }, use) => {
+    await use(new LeftNavigationPage(page));
   },
 });
