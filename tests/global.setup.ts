9fa6ca888bdfdc5554351e9705926edbcd1f1561
Amarendra91 <amarendra867@gmail.com>
2025-07-01 22:46:08 +0530

    Added comments

--- tests/global.setup.ts
+++ tests/global.setup.ts
@@ -1,19 +1,21 @@
 import { test } from '../fixtures/common-fixture';
 
-test('Login session storage for reusability.', async ({
-  page,
-  homePage,
-  commonUtils,
-}) => {
+test('Save login session storage.', async ({ page, homePage, commonUtils }) => {
   const decryptedUserName = commonUtils.decryptData(process.env.USER_NAME!);
   const decryptedPassword = commonUtils.decryptData(process.env.PASSWORD!);
-
+  // Navigate to login page
   await homePage.goToLoginPage();
   await page.waitForURL(`${process.env.BASE_URL}/web/index.php/auth/login`);
+
+  // Perform login with valid credentials
   await homePage.login(decryptedUserName, decryptedPassword);
+
+  // Validate Dashboard page Navigation after successful login
   await page.waitForURL(
     `${process.env.BASE_URL}/web/index.php/dashboard/index`
   );
+
+  // Save login session storage state
   await page.context().storageState({
     path: './.auth/auth.json',
   });
