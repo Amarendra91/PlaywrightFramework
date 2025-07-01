f65cc2ffc1e1e134bfb24aba4ab0cfec0ae0df0e
Amarendra91 <amarendra867@gmail.com>
2025-07-01 22:48:39 +0530

    moved the test files into web folder under tests folder

---
+++ tests/web/dashboard-module.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from '../../fixtures/hooks-fixture';
+import dashboardModuleTestData from '../../test-data/dashboard-module.json';
+
+test.describe('Verify Dashbaoard page functionality.', async () => {
+  test(
+    'Verify user navigated to OrangeHRM dashbaord page and logout from OrangeHRM application.',
+    { tag: '@Smoke' },
+    async ({ bypassLoginPage, dashboardpage, homePage }) => {
+      await test.step('Verify header title on dashboard page.', async () => {
+        await expect(dashboardpage.headerTitle).toContainText(
+          dashboardModuleTestData.header_title
+        );
+      });
+
+      await test.step('Verify user dropdown menu functionality on page header.', async () => {
+        await expect(dashboardpage.userIconMenu).toBeVisible();
+        await expect(dashboardpage.userIconMenu).toBeEnabled();
+      });
+
+      await test.step('Verify Logout functionality. ', async () => {
+        await dashboardpage.logout();
+        await expect(homePage.loginHeader).toBeVisible();
+      });
+    }
+  );
+});
