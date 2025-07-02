3126eb19b9188ac76d214d0e40441c02af7b9c9c
Amarendra91 <amarendra867@gmail.com>
2025-07-02 23:47:46 +0530

    updated json reporter for CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,10 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI
-    ? [['html'], ['json', { outputFile: 'ctrf/results.json' }]]
+    ? [
+        ['html'],
+        ['playwright-ctrf-json-reporter', { outputFile: 'ctrf/results.json' }],
+      ]
     : [['html', { open: 'always' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
