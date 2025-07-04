726c82af73c4432682b94431e0900db87fd8080a
Amarendra91 <amarendra867@gmail.com>
2025-07-04 19:24:25 +0530

    file name change

---
+++ utils/CryptoUtils.ts
@@ -0,0 +1,39 @@
+import cryptoJs from 'crypto-js';
+
+export default class CryptoUtils {
+  private secretKey: string;
+
+  /**
+   *Initilizing secret key
+   */
+  constructor() {
+    if (process.env.SECRET_KEY) {
+      this.secretKey = process.env.SECRET_KEY;
+    } else {
+      throw new Error('Please provide Secret key while starting execution.');
+    }
+  }
+
+  /**
+   * Create encrypted data from normal sting data and convert it to string
+   * @param data
+   * @returns encryptedData
+   */
+  public encryptData(data: string) {
+    const encryptedData = cryptoJs.AES.encrypt(data, this.secretKey).toString();
+    return encryptedData;
+  }
+
+  /**
+   * Create decrypted data from encrypted sting data and convert it to string
+   * @param encData
+   * @returns decryptedData
+   */
+  public decryptData(encData: string) {
+    const decryptedData = cryptoJs.AES.decrypt(
+      encData,
+      this.secretKey
+    ).toString(cryptoJs.enc.Utf8);
+    return decryptedData;
+  }
+}
