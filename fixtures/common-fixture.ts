a9630c385d3bdfcea7ed132032a5ff04e8a7e6f2
Amarendra91 <amarendra867@gmail.com>
2025-07-07 17:32:58 +0530

    added fixture for auth token generation util for api tests

--- fixtures/common-fixture.ts
+++ fixtures/common-fixture.ts
@@ -1,12 +1,17 @@
 import { test as basetest } from './pom-fixture';
 import CommonUtils from '../utils/CommonUtils';
+import CommonApiUtils from '../utils/CommonApiUtils';
 
 type CommonFixtureType = {
   commonUtils: CommonUtils;
+  commonApiUtils: CommonApiUtils;
 };
 
 export const test = basetest.extend<CommonFixtureType>({
   commonUtils: async ({}, use) => {
     await use(new CommonUtils());
+  },
+  commonApiUtils: async ({ request }, use) => {
+    await use(new CommonApiUtils(request));
   },
 });
